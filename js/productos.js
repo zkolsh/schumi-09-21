26ec333b089cb915d269add7e8ab3c82deb4a1c9
BenicioPoli <beniciosanchezmandato@gmail.com>
2025-09-20 12:08:29 -0300

    puesto ruta de imagne

--- js/productos.js
+++ js/productos.js
@@ -3,25 +3,25 @@ const listaDeTortas = [
     nombre: "Torta Toffe",
     descripcion: "Exquisita Torta Toffe cubierta con chocolate tortres hecho con el cacao más suave.",
     precio: "$15000",
-    imagen: "torta-toffee.jpg"
+    imagen: "img/torta-toffee.jpg"
   },
   {
     nombre: "Chocotorta",
     descripcion: "Chocotorta cubierta con delicioso chocolate en rama.",
     precio: "$20000",
-    imagen: "chocotorta.jpg"
+    imagen: "img/chocotorta.jpg"
   },
   {
     nombre: "LemonPie",
     descripcion: "LemonPie hecho con los mejores limones de Santa Fe y con una dulce crema Chantilly.",
     precio: "$15000",
-    imagen: "lemon-pie.jpg"
+    imagen: "img/lemon-pie.jpg"
   },
   {
     nombre: "Torta Argentina",
     descripcion: "Mirá el partido de Argentina con esta hermosa torta de bizcochuelo blanco y negro relleno con el mejor Dulce de Leche y decorada como nuestra enseña patria.",
     precio: "$10000",
-    imagen: "tortaargentina.jpg"
+    imagen: "img/tortaargentina.jpg"
   }
 ];
 
@@ -53,3 +53,4 @@ listaDeTortas.forEach(torta => {
 
   contenedor.appendChild(tarjeta);
 });
+
