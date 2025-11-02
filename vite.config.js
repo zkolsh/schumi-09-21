8a768a7ddee60a1d795622906b91504c92c46cad
zkolsh <129521109+zkolsh@users.noreply.github.com>
2025-11-02 20:52:54 +0000

    admin & backend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				admin: resolve(__dirname, 'admin.html')
+			}
+		}
+	},
 	plugins: [
 		tailwindcss(),
 	],
-	base: '/schumi-09-21/',
+	base: '/~veinticinco/',
 })
